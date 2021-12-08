072cba059a9fcf46b42a06b6066c6bb2b4554acc
LONGSIGn <silong170214@gmail.com>
2021-12-08 12:03:16 +0100

    注释内容
    echarts 注释

--- app/src/main/java/com/isep/group4/android_weather_forecast/MainActivity.java
+++ app/src/main/java/com/isep/group4/android_weather_forecast/MainActivity.java
@@ -29,9 +29,36 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }).launch(Manifest.permission.ACCESS_FINE_LOCATION);
 
+        //        lineChart = findViewById(R.id.lineChart);
+//        lineChart.setWebViewClient(new WebViewClient(){
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//                super.onPageFinished(view, url);
+//                //最好在h5页面加载完毕后再加载数据，防止html的标签还未加载完成，不能正常显示
+//                refreshLineChart();
+//            }
+//        });
+
     }
+
+//    private void refreshLineChart(){
+//        Object[] x = new Object[]{
+//                "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"
+//        };
+//        Object[] y = new Object[]{
+//                820, 932, 901, 934, 1290, 1330, 1320
+//        };
+//        lineChart.refreshEchartsWithOption(echartOption.getLineChartOptions(x, y));
+//    }
+
+
+
 
     public void updateCurrentWeather() {
 
     }
+
+
+
+
 }
